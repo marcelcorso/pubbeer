7990fbadaa0ff00068ca01d030eeb25617e2d82b
Marcel Corso Gonzalez <marcel@darth-marcel.(none)>
2010-02-14 11:45:00 +0100

    added some listing functions

--- pubbeer.erl
+++ pubbeer.erl
@@ -3,7 +3,7 @@
 -behaviour(gen_server).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
--export([start_link/0, create/1, destroy/1, pub/2, sub/2, unsub/2, shutdown/0]).
+-export([start_link/0, create/1, destroy/1, pub/2, sub/2, unsub/2, list_nodes/0, list_subscribers/1, shutdown/0]).
 
 %% Client functions
 start_link() ->
@@ -13,7 +13,10 @@ create(NodeName) ->
   gen_server:call(?MODULE, {create, NodeName}).
 
 destroy(NodeName) ->
-  gen_server:call(?MODULE, {create, NodeName}).
+  gen_server:call(?MODULE, {destroy, NodeName}).
+
+list_nodes() ->
+  gen_server:call(?MODULE, {list_nodes}).
 
 pub(NodeName, Body) ->
   gen_server:call(?MODULE, {pub, NodeName, Body}).
@@ -23,6 +26,9 @@ sub(NodeName, Subscriber) ->
 
 unsub(NodeName, Subscriber) ->
   gen_server:call(?MODULE, {unsub, NodeName, Subscriber}).
+
+list_subscribers(NodeName) -> 
+  gen_server:call(?MODULE, {list_subscribers, NodeName}).
 
 shutdown() ->
   gen_server:cast(?MODULE, stop).
@@ -49,7 +55,11 @@ handle_call({destroy, NodeName}, _From, Nodes) ->
     error ->
       {reply, false, Nodes}
   end;
-  
+ 
+handle_call({list_nodes}, _From, Nodes) ->
+  {reply, dict:fetch_keys(Nodes), Nodes};
+
+ 
 handle_call({pub, NodeName, Body}, _From, Nodes) ->
   case dict:find(NodeName, Nodes) of
     {ok, NodeListeners} ->
@@ -76,7 +86,7 @@ handle_call({sub, NodeName, Subscriber}, _From, Nodes) ->
     {ok, NodeListeners} ->
       case lists:member(Subscriber, NodeListeners) of 
         true ->
-          {reply, ok, Nodes};
+         {reply, ok, Nodes};
         false ->
           {reply, ok, dict:store(NodeName, [Subscriber|NodeListeners], Nodes)}
       end;
@@ -89,7 +99,7 @@ handle_call({unsub, NodeName, Subscriber}, _From, Nodes) ->
     {ok, NodeListeners} ->
       case lists:member(Subscriber, NodeListeners) of 
         true ->
-          {reply, ok, dict:store(NodeName, Nodes, list:delete(Subscriber, NodeListeners))};
+          {reply, ok, dict:store(NodeName, lists:delete(Subscriber, NodeListeners), Nodes)};
         false ->
           {reply, false, Nodes}
       end;
@@ -97,7 +107,14 @@ handle_call({unsub, NodeName, Subscriber}, _From, Nodes) ->
       {reply, false, Nodes}
   end;  
   
-  
+handle_call({list_subscribers, NodeName}, _From, Nodes) ->
+  case dict:find(NodeName, Nodes) of
+    {ok, NodeListeners} ->
+      {reply, NodeListeners, Nodes};
+    error ->
+      {reply, false, Nodes}
+  end;
+
 handle_call(_Request, _From, State) ->
   {reply, ignored, State}.
 
